631aaf3b8603fe7729bc75d9d7387b88640fcf13
avimi13 <alfredorequenez57libra@gmail.com>
2022-09-05 13:43:18 -0600

    Update

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React, {useState}from 'react';
 import {View} from 'react-native';
 import {AppBar} from './components/AppVar'
-import {Pharmacy} from './Pharmacy'
+import {Pharmacy} from './components/Pharmacy'
 
 const App = () => {
   return (
